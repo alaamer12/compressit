28600f0c527ee757430d21343ec99ead385eff54
alaamer12 <ahmedmuhmmed239@gmail.com>
2024-05-27 22:53:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+this project is a practice on how can u compress and decompress a file, string, etc.
+with an algorithm of our own 
+
+in this project we will use huffman algorithm to compress and decompress.
+
+ we will also use special custom file extension from our own we will call it .huff
+    
---
+++ main.py
@@ -0,0 +1,26 @@
+class CompressIt:
+    def __init__(self):
+        pass
+
+    def compress(self):
+        pass
+
+    def decompress(self):
+        pass
+
+    def decode(self):
+        pass
+
+    def encode(self):
+        pass
+
+    def get_size(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
